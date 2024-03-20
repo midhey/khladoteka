6ae27f17b9aa6d0d6ac431a78c297bb4dae0c1b2
Alexey <midheyss@gmail.com>
2024-03-20 18:10:45 +0300

    Обновлен баннер: •Разработана десктоп версия •Разработана мобильная версия

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,17 +9,19 @@ import './gallery-modal.js'
 
 
 
-
-
 const dropdownBtns = document.querySelectorAll('.dropdown-btn');
 const dropdowns = document.querySelectorAll('.dropdown');
 const desktopTriggers = document.querySelectorAll('.desktop-trigger');
-const dropdownIcons = document.querySelectorAll('.dropdown-icon')
+const dropdownIcons = document.querySelectorAll('.dropdown-icon');
 
 // Функция для открытия/закрытия списка
 function toggleDropdown(index) {
     dropdowns[index].classList.toggle('active');
-    dropdownIcons[index].classList.toggle('icon-rotate');
+    if (dropdowns[index].classList.contains('active')) {
+        dropdownIcons[index].classList.add('icon-rotate');
+    } else {
+        dropdownIcons[index].classList.remove('icon-rotate');
+    }
 }
 
 // Обработчики событий для каждой кнопки
@@ -33,22 +35,21 @@ desktopTriggers.forEach((trigger, index) => {
 
 dropdownBtns.forEach((btn, index) => {
     btn.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращаем переход по ссылке
-        if (window.innerWidth < 990) {
-            toggleDropdown(index);
+        event.preventDefault();
+        const isMenuBtn = event.currentTarget.contains(event.target); // Проверяем, был ли клик на самой кнопке
+        if (!isMenuBtn) return; // Если клик был не на кнопке, прекращаем выполнение функции
+        toggleDropdown(index);
+        const checkbox = document.getElementById('top-menu-btn'); // Получаем элемент чекбокса
+        if (btn.closest('.menu-icon')) { // Проверяем, находится ли кнопка внутри .menu-icon
+            checkbox.checked = false; // Устанавливаем состояние чекбокса на Unchecked
         }
-        event.stopPropagation(); // Остановка всплытия события
+        event.stopPropagation(); // Останавливаем всплытие события, чтобы оно не передавалось родительским элементам
     });
 });
 
-// Закрытие списка при клике вне его области
-document.addEventListener('click', (event) => {
-    dropdownBtns.forEach((btn, index) => {
-        if (!btn.contains(event.target) && !dropdowns[index].contains(event.target)) {
-            dropdowns[index].classList.remove('active');
-        }
-    });
-});
+
+
+
 
 
 
