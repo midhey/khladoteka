32c4c9867028ef619d4dd59b3e850f3c0ad6302b
Alexey <midheyss@gmail.com>
2024-03-20 12:29:28 +0300

    Обновлен блок преимуществ: • Добавлено раскрытие преимущества • Добавлены анимации

--- src/scripts/articles.js
+++ src/scripts/articles.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const articlesBlocks = document.querySelectorAll('.articles-block');
 
-    articlesBlocks.forEach(function(articlesBlock) {
+    articlesBlocks.forEach(function(articlesBlock, index) {
         const articlesBlockText = articlesBlock.querySelector('.articles-block-text');
 
         articlesBlock.addEventListener('click', function(event) {
             // Переключаем класс для раскрытия или скрытия блока текста
             articlesBlock.classList.toggle('articles-block-expanded');
+            // Если это последний элемент, добавляем класс для открытия влево
+            if (index === articlesBlocks.length - 1) {
+                articlesBlock.classList.toggle('articles-block-left');
+            }
             event.stopPropagation(); // Останавливаем всплытие события
         });
 
         // Скрываем текст при любом клике в документе
         document.addEventListener('click', function() {
             articlesBlock.classList.remove('articles-block-expanded');
+            // Удаляем класс для открытия влево
+            articlesBlock.classList.remove('articles-block-left');
         });
     });
 
