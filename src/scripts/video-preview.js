f0fab7f7cd24cf58868e3132a7892750b4fd969d
midhey <midheyss@gmail.com>
2024-03-22 03:08:01 +0300

    Обновлен блок "Наша цель": •Добавлена фигура •Добавлен адаптив

--- src/scripts/video-preview.js
+++ src/scripts/video-preview.js
@@ -1,38 +1,38 @@
-// main.js
-let player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        playerVars : {
-            autoplay: 0,
-            controls: 0,
-            rel: 0,
-            showinfo: 0,
-            start: 15,
-            iv_load_policy: 3,
-            origin: location.hostname
-        },
-        videoId: 'Rd5Aa7Uliy8',
-        events: {
-            'onReady': onPlayerReady
-        },
-    });
-}
-
-function onPlayerReady(event) {
-    var playPauseBtn = document.getElementById('playPauseBtn');
-    playPauseBtn.addEventListener('click', function() {
-        if (player.getPlayerState() == YT.PlayerState.PLAYING) {
-            player.pauseVideo();
-        } else {
-            player.playVideo();
-        }
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Инициализируем YouTube IFrame API при загрузке DOM
-    onYouTubeIframeAPIReady();
-});
+// // main.js
+// let player;
+//
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '100%',
+//         width: '100%',
+//         playerVars : {
+//             autoplay: 0,
+//             controls: 0,
+//             rel: 0,
+//             showinfo: 0,
+//             start: 15,
+//             iv_load_policy: 3,
+//             origin: location.hostname
+//         },
+//         videoId: 'Rd5Aa7Uliy8',
+//         events: {
+//             'onReady': onPlayerReady
+//         },
+//     });
+// }
+//
+// function onPlayerReady(event) {
+//     var playPauseBtn = document.getElementById('playPauseBtn');
+//     playPauseBtn.addEventListener('click', function() {
+//         if (player.getPlayerState() == YT.PlayerState.PLAYING) {
+//             player.pauseVideo();
+//         } else {
+//             player.playVideo();
+//         }
+//     });
+// }
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Инициализируем YouTube IFrame API при загрузке DOM
+//     onYouTubeIframeAPIReady();
+// });
